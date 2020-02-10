38db47739c87cc84639a2505290c837fc4376fc3
Robbie Hill <rhill53@msudenver.edu>
2020-02-10 15:37:34 -0700

    fully working with file generation

--- src/sort/Main.java
+++ src/sort/Main.java
@@ -6,7 +6,7 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        int[] originalArry = RandomArray.generateArray(1000);
+        int[] originalArry = RandomArray.generateArray(1000000);
         int n = originalArry.length;
         int writeNum = 1;
         Scanner input = new Scanner(System.in);
